0e28d5b01e3d1eb0f56bd5a69d891cd6a344a8ef
Woody <60283977+YH3041@users.noreply.github.com>
2022-03-12 18:38:30 +0900

    나머지 매개변수

--- part1/036.js
+++ part1/036.js
@@ -2,6 +2,7 @@ function myFun(a, b, ...args) {
     console.log("a = " + a);
     console.log("b = " + b);
     console.log("args = " + args);
+    console.log("");
 };
 
 myFun("one", "two", "three", "four", "five");
