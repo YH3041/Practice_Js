a5e7ce4a35aa96f99c79999bdb4f3f95036b6d26
Woody <60283977+YH3041@users.noreply.github.com>
2022-03-13 14:29:46 +0900

    test

--- part1/036.js
+++ part1/036.js
@@ -2,7 +2,6 @@ function myFun(a, b, ...args) {
     console.log("a = " + a);
     console.log("b = " + b);
     console.log("args = " + args);
-    console.log("");
 };
 
 myFun("one", "two", "three", "four", "five");
